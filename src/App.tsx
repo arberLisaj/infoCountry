1eac9177549560a25ef6f5134b3c41e5bf567e01
arberlisaj <arberlisaj1@gmail.com>
2023-07-12 16:46:46 +0200

    add: favicon and filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import "./styles/styles.css";
+import FilterCountries from "./components/FilterCountries";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
   return (
     <main className={darkMode ? "dark-mode" : "light-mode"}>
       <Header darkMode={darkMode} setDarkMode={(data) => setDarkMode(data)} />
+      <FilterCountries />
     </main>
   );
 };
--- src/components/FilterCountries.tsx
+++ src/components/FilterCountries.tsx
@@ -1,9 +1,21 @@
+import { AiOutlineSearch } from "react-icons/ai";
+
 const FilterCountries = () => {
   return (
     <section id="filter">
-      <input type="text" />
+      <div className="inputBox">
+        <span>
+          <AiOutlineSearch />
+        </span>
+        <input type="text" placeholder="Search for a country..." />
+      </div>
       <select>
+        <option value="">Filter by Region</option>
         <option value="africa">africa</option>
+        <option value="america">america</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
       </select>
     </section>
   );
