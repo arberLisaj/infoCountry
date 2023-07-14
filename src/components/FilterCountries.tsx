a81217e4c008739766247abbe2b262852d5f6028
arberlisaj <arberlisaj1@gmail.com>
2023-07-14 10:20:11 +0200

    add: additional updates

--- src/components/FilterCountries.tsx
+++ src/components/FilterCountries.tsx
@@ -7,17 +7,18 @@ interface Props {
 const FilterCountries = ({ setData, setInputValue }: Props) => {
   return (
     <section id="filter">
-      <div className="inputBox">
+      <section className="inputBox">
         <span>
           <AiOutlineSearch />
         </span>
         <input
+          id="search"
           name="search"
           onChange={(e) => setInputValue(e.target.value)}
           type="text"
           placeholder="Search for a country..."
         />
-      </div>
+      </section>
       <select onChange={(option) => setData(option.target.value)}>
         <option value="">All Regions</option>
         <option value="Africa">africa</option>
