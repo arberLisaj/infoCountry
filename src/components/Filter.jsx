f9b9afa4bd8b4b713110abb470af1968210728c0
arberlisaj <arberlisaj1@gmail.com>
2023-11-23 21:32:05 +0100

    fix input overlap

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -32,6 +32,7 @@ function Filter({ inputValue, setInputValue, region, setRegion }) {
           placeholder="Search a country..."
           style={{
             border: "none",
+            width: "100%",
             padding: "7px 3px",
           }}
         />
